eb2f42134665951a95068ea2c2c643d872776153
Atte <naquden@gmail.com>
2015-02-01 18:54:44 +0100

    Clean up

--- app/src/main/java/com/atte/kramamanda/ui/MainActivity.java
+++ app/src/main/java/com/atte/kramamanda/ui/MainActivity.java
@@ -3,39 +3,19 @@
  */
 package com.atte.kramamanda.ui;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.atte.kramamanda.R;
 
 /**
  * Main activity. Contains the fragments.
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        initTestViews();
-    }
-
-    /**
-     * Method to initialize elements for test purpose.
-     * TODO: remove this method and following test elements
-     */
-    private void initTestViews() {
-        // Test Notification
-        findViewById(R.id.btn_test_notification).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                NotificationService.scheduleNotification(
-                        MainActivity.this,
-                        NotificationService.NOTIFICATION_ID_HUG_WAITING_ID,
-                        (System.currentTimeMillis() + 5000));
-            }
-        });
     }
 }
