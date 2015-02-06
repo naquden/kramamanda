36a1c1e966aa532fc9ef1744ae323853aa12cd5b
Atte <naquden@gmail.com>
2015-02-07 00:40:18 +0100

    Update search url
    Changed image search url from google to yahoo.
    Yahoo provides  triple amount of images and also larger ones.

--- app/src/main/java/util/KramConstant.java
+++ app/src/main/java/util/KramConstant.java
@@ -13,7 +13,7 @@ public class KramConstant {
 
     public static final String ACTION_REQUEST_HUG = "com.backenhof.action.REQUEST_HUG";
 
-    public static final String GOOGLE_SEARCH_URL = "https://www.google.se/search?q=hug&biw=1496&bih=901&site=hug&source=lnms&tbm=isch&sa=X&ei=t1bOVNrFBYfVywOKrYFA&ved=0CAYQ_AUoAQ";
+    public static final String GOOGLE_SEARCH_URL = "https://se.images.search.yahoo.com/search/images;_ylt=A9mSs2nER9VUKwoAr3ETOAx.?p=hug&fr=yfp-t-925&fr2=piv-web";
 
     // Url to the html, exclude the .html ext to support a fixed number of pages.
     public static final String QUOTES_URL = "http://www.brainyquote.com/quotes/keywords/good_day";
